bf8096b9ca4fe63bf9fb5f6e12a175efcfc98ba8
Usuario <Usuario@PC-JP>
2024-03-04 20:56:36 -0600

    Semana #6: Commit del proyecto Tienda.

---
+++ src/main/java/com/tienda/service/FirebaseStorageService.java
@@ -0,0 +1,20 @@
+package com.tienda.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface FirebaseStorageService {
+
+    public String cargaImagen(MultipartFile archivoLocalCliente, String carpeta, Long id);
+
+    //El BuketName es el <id_del_proyecto> + ".appspot.com"
+    final String BucketName = "techshop-c9e80.appspot.com";
+
+    //Esta es la ruta básica de este proyecto Techshop
+    final String rutaSuperiorStorage = "techshop";
+
+    //Ubicación donde se encuentra el archivo de configuración Json
+    final String rutaJsonFile = "firebase";
+    
+    //El nombre del archivo Json
+    final String archivoJsonFile = "techshop-c9e80-firebase-adminsdk-wckn2-355b4eaccd.json";
+}
